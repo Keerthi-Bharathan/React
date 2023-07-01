95062b56e1d6ddce0b4cc38858db375960f29052
Keerthi-Bharathan <keerthikv2710@gmail.com>
2023-07-01 11:16:05 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import kk from "../assets/download.png"
+
+const Home = () => {
+  return (
+    <>
+    <div>Home</div>
+    <div className='image'>
+        <img src={kk} alt="" />
+    </div>
+    </>
+  )
+}
+
+export default Home
