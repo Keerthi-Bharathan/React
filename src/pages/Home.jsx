f56dcbd9eedf56d3246f9fbdf8b5fba38062140e
Keerthi-Bharathan <keerthikv2710@gmail.com>
2023-07-05 18:01:00 +0530

    spotify

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,31 @@
 import React from 'react'
-import kk from "../assets/download.png"
+import {Link} from 'React-Router-dom'
+import image from '../assets/spotify.png'
+import './Home.css'
 
-const Home = () => {
+
+const Home = () => { 
   return (
     <>
-    <div>Home</div>
-    <div className='image'>
-        <img src={kk} alt="" />
+    <div className='container'>
+      <div className='img'>
+        <img src={image} /></div>
+      <div className='a'>Premium</div>
+      <div className='b'>Help</div>
+      <div className='c'>Download</div>
+      <div className='d'>|</div>
+      <div className='e'>Sign up</div>
+      <Link to={"/Login"}>
+        <div className='f'>Log In</div></Link>
+      
     </div>
-    <div>keerthi</div>
+    <div className='container1'>
+      <div className='g'>Music for everyone.</div>
+      <div className='h'>Millions of songs.No credit card neede.</div>
+      
+      <button>GET SPOTIFY FREE</button>
+    
+    </div>
     </>
   )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import './Login.css'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <>
+    <div className='container3'>
+      <form id='log'>
+        <h1>Login</h1>
+        <label htmlFor='email'>Email:</label><br />
+        <input type='email' id='email' placeholder='Enter email'/><br /><br />
+        <label htmlFor='password'>Password:</label><br />
+        <input type='password' id='password' placeholder='Enter password'/><br /><br />
+        <input type='checkbox' id='check'/>
+        <label htmlFor='Show'>Show Password</label><br />
+        <button type='SIGN IN' id='Sign'>SIGN IN</button><br /><br />
+        <div className='ab'>Forgot <a href='none'>Username/Password</a></div><br />
+        <div className='bc'>Don't have an account? <a href='none'>Sign up</a></div>
+
+      </form>
+      
+
+    
+    </div>
+    </>
+   
   )
 }
 
